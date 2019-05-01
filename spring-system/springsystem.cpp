b688e768ee828ff2bc830a1f34578afcca518275
Dyakov Roman <dyakovri@gmail.com>
2019-05-01 21:27:07 +0500

    Updating equations

--- spring-system/springsystem.cpp
+++ spring-system/springsystem.cpp
@@ -2,7 +2,7 @@
 
 
 
-springsys::springsys(double m, double M, double k1, double k2, double k3, double k4, double mu, double x0, double y0) : RungeKutta(8)
+springsys::springsys(double m, double M, double k1, double k2, double k3, double k4, double mu, double x0, double y0, double w, double h) : RungeKutta(8)
 {
 	this->k1 = k1;
 	this->k2 = k2;
@@ -11,6 +11,9 @@ springsys::springsys(double m, double M, double k1, double k2, double k3, double
 
 	this->m = m;
 	this->M = M;
+
+	this->w = w;
+	this->h = h;
 
 	this->mu = mu;
 
@@ -26,6 +29,14 @@ springsys::~springsys()
 {
 }
 
+double springsys::Ftr(double mu, double m, double max)
+{
+	double F = mu * m * 9.81;
+	max = abs(max);
+	if (F > max) return -max;
+	return -F;
+}
+
 std::vector<double> springsys::F(double t, std::vector<double>& Y)
 {
 	FY[0] = Y[4]; // x' = Vx
@@ -33,24 +44,39 @@ std::vector<double> springsys::F(double t, std::vector<double>& Y)
 	FY[2] = Y[6]; // X' = VX
 	FY[3] = Y[7]; // Y' = VY
 
+	// Длины пружин
+	double l1 = sqrt(pow(Y[0] - Y[2], 2) + pow(Y[1] - Y[3] - h, 2));
+	double l2 = sqrt(pow(Y[0] - Y[2] - w, 2) + pow(Y[1] - Y[3], 2));
+	double l3 = sqrt(pow(Y[0] - Y[2], 2) + pow(Y[1] - Y[3] + h, 2));
+	double l4 = sqrt(pow(Y[0] - Y[2] + w, 2) + pow(Y[1] - Y[3], 2));
 
+	// Удлинения пружин
+	double d1 = l1 - w;
+	double d2 = l2 - h;
+	double d3 = w - l3;
+	double d4 = h - l4;
+
+	// Силы упругости, действующие вдоль осей x и y
+	double Fx = Y[0] * (d1 * k1 / l1) + (w - Y[0]) * (d2 * k2 / l2) + Y[0] * (d3 * k3 / l3) + (w - Y[0]) * (d4 * k4 / l4);
+	double Fy = (h - Y[1]) * (d1 * k1 / l1) + Y[1] * (d2 * k2 / l2) + (h - Y[1]) * (d3 * k3 / l3) + Y[1] * (d4 * k4 / l4);
+
+	// Рассчет синуса и косинуса направления скорости для проецирования силы трения
 	double vvx = Y[6] / sqrt(Y[6] * Y[6] + Y[7] * Y[7]);
 	double vvy = Y[7] / sqrt(Y[6] * Y[6] + Y[7] * Y[7]);
 	if ((Y[6] == 0) || (Y[7] == 0)) {
 		vvx = 0; 
 		vvy = 0;
 	}
+	
+	// Рассчет сил трения
+	double Ftr_x = Ftr(mu, m + M, Fx) * vvx;
+	double Ftr_y = Ftr(mu, m + M, Fy) * vvy;
 
-	double Fup_x = Fup(k1+k3, Y[0]);
-	double Fup_y = Fup(k2 + k4, Y[1]);
-	double Ftr_x = Ftr(mu, m + M, Fup_x) * vvx;
-	double Ftr_y = Ftr(mu, m + M, Fup_y) * vvy;
+	FY[6] = (-Fx + Ftr_x) / (M); // VX' = -Fупругости_x + Fтрения_x  / M
+	FY[7] = (-Fy + Ftr_y) / (M); // VY' = -Fупругости_y + Fтрения_y  / M
 
-	FY[6] = (Fup_x + Ftr_x) / M;
-	FY[7] = (Fup_y + Ftr_y) / M;
-
-	FY[4] = (-Fup_x - M * FY[6]) / m;
-	FY[5] = (-Fup_y - M * FY[7]) / m;
+	FY[4] = Fx / m; // Vx' = Fупругости_x / m
+	FY[5] = Fy / m; // Vy' = Fупругости_y / m
 	
 
 	return FY;
--- spring-system/springsystem.h
+++ spring-system/springsystem.h
@@ -7,24 +7,20 @@ class springsys :
 private:
 	double k1, k2, k3, k4;
 	double m, M;
+	double w, h;
 	double mu;
 
 public:
-	springsys(double m, double M, double k1, double k2, double k3, double k4, double mu, double x0, double y0);
+	springsys(double m, double M, double k1, double k2, double k3, double k4, double mu, double x0, double y0, double w, double h);
 	~springsys();
 
-	double get_x() { return (Y[0] + Y[2]); }
-	double get_y() { return (Y[1] + Y[3]); }
-	double get_X() { return (Y[2]); }
-	double get_Y() { return (Y[3]); }
+	double get_x() { return Y[0]; }
+	double get_y() { return Y[1]; }
+	double get_X() { return Y[2]; }
+	double get_Y() { return Y[3]; }
 
 	double Fup(double k, double dx) { return k * dx; }
-	double Ftr(double mu, double m, double max) { 
-		double F = mu * m * 9.81;
-		max = abs(max);
-		if (F > max) return -max;
-		return -F; 
-	}
+	double Ftr(double mu, double m, double max);
 
 	// Унаследовано через RungeKutta
 	virtual std::vector<double> F(double t, std::vector<double>& Y) override;
