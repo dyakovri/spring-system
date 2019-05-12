bc6a18a2aa505f72f7516bd5ce12f8ebbf0490d5
dyakovri <errorman.gamer@gmail.com>
2019-05-12 19:38:04 +0300

    Equations redeveloped

--- spring-system/springsystem.cpp
+++ spring-system/springsystem.cpp
@@ -29,12 +29,39 @@ springsys::~springsys()
 {
 }
 
-double springsys::Ftr(double mu, double m, double max)
+double springsys::Fdx(double mu, double m, double max)
 {
-	double F = mu * m * 9.81;
-	max = abs(max);
-	if (F > max) return -max;
-	return -F;
+	double Fd = mu * m * 9.81;
+	
+	if ((Y[6] != 0) && (Y[7] != 0))
+		Fd = Fd * Y[6] / sqrt(Y[6] * Y[6] + Y[7] * Y[7]);
+	else 
+		Fd = 0;
+	
+	if (Y[6] == 0) 
+		if (Fd > max)
+			return -max;
+	
+	return -Fd;
+
+}
+
+
+double springsys::Fdy(double mu, double m, double max)
+{
+	double Fd = mu * m * 9.81;
+
+	if ((Y[6] != 0) && (Y[7] != 0))
+		Fd = Fd * Y[7] / sqrt(Y[6] * Y[6] + Y[7] * Y[7]);
+	else
+		Fd = 0;
+
+	if (Y[7] == 0) 
+		if (Fd > max) 
+			return -max;
+
+	return -Fd;
+
 }
 
 std::vector<double> springsys::get_spring_begin(int num)
@@ -68,61 +95,40 @@ std::vector<double> springsys::get_spring_begin(int num)
 
 std::vector<double> springsys::F(double t, std::vector<double>& Y)
 {
+	double x = Y[0];
+	double y = Y[1];
+
 	FY[0] = Y[4]; // x' = Vx
 	FY[1] = Y[5]; // y' = Vy
 	FY[2] = Y[6]; // X' = VX
 	FY[3] = Y[7]; // Y' = VY
 
-	// Длины пружин
-	/*double l1 = sqrt(pow(Y[0] - Y[2], 2) + pow(Y[1] - Y[3] - h, 2));
-	double l2 = sqrt(pow(Y[0] - Y[2] - w, 2) + pow(Y[1] - Y[3], 2));
-	double l3 = sqrt(pow(Y[0] - Y[2], 2) + pow(Y[1] - Y[3] + h, 2));
-	double l4 = sqrt(pow(Y[0] - Y[2] + w, 2) + pow(Y[1] - Y[3], 2));*/
-
-	double l1 = sqrt(pow(Y[0] - get_spring_begin(0)[0], 2) + pow(Y[1] - get_spring_begin(0)[1], 2));
-	double l2 = sqrt(pow(Y[0] - get_spring_begin(1)[0], 2) + pow(Y[1] - get_spring_begin(1)[1], 2));
-	double l3 = sqrt(pow(Y[0] - get_spring_begin(2)[0], 2) + pow(Y[1] - get_spring_begin(2)[1], 2));
-	double l4 = sqrt(pow(Y[0] - get_spring_begin(3)[0], 2) + pow(Y[1] - get_spring_begin(3)[1], 2));
-
-
-	// Удлинения пружин
-	double d1 = l1 - w;
-	double d2 = l2 - h;
-	double d3 = w - l3;
-	double d4 = h - l4;
-
-	// Силы упругости, действующие вдоль осей x и y
-	double F1x = k1 * (Y[0] - Y[2]) * ((h) / sqrt(pow(Y[0] - Y[2], 2) + pow(Y[3] + h - Y[1], 2)) - 1);
-	double F2x = -k2 * (Y[2] - Y[0] + w) * ((w) / sqrt(pow(Y[2] - Y[0] + w, 2) + pow(Y[1] - Y[3], 2)));
-	double F3x = -k3 * (Y[0] - Y[2]) * (1 - (h) / sqrt(pow(Y[0] - Y[2], 2) + pow(Y[1] + h - Y[3], 2)));
-	double F4x = -k4 * (Y[2] - Y[0] + w) * (1 - (w) / sqrt(pow(Y[0] - Y[2] + w, 2) + pow(Y[1] - Y[3], 2)));
-
-	double F1y = -k1 * (Y[3] - Y[1] + h) * ((h) / sqrt(pow(Y[0] - Y[2], 2) + pow(Y[3] + h - Y[1], 2)) - 1);
-	double F2y = k2 * (Y[1] - Y[3]) * ((w) / sqrt(pow(Y[2] - Y[0] + w, 2) + pow(Y[1] - Y[3], 2)));
-	double F3y = -k3 * (Y[1] - Y[3] + h) * (1 - (h) / sqrt(pow(Y[0] - Y[2], 2) + pow(Y[1] + h - Y[3], 2)));
-	double F4y = -k4 * (Y[1] - Y[3]) * (1 - (w) / sqrt(pow(Y[0] - Y[2] + w, 2) + pow(Y[1] - Y[3], 2)));
-
-	double Fx = F1x + F2x + F3x + F4x;
-	double Fy = F1y + F2y + F3y + F4y;
-
-	// Рассчет синуса и косинуса направления скорости для проецирования силы трения
-	double vvx = Y[6] / sqrt(Y[6] * Y[6] + Y[7] * Y[7]);
-	double vvy = Y[7] / sqrt(Y[6] * Y[6] + Y[7] * Y[7]);
-	if ((Y[6] == 0) || (Y[7] == 0)) {
-		vvx = 0; 
-		vvy = 0;
+	/*double Fd = mu*(M + m)*9.81;
+	double Fdx, Fdy;
+	if ((Fdx != 0) && (Fdy != 0)) {
+		Fdx = Fd * Y[6] / sqrt(Y[6] * Y[6] + Y[7] * Y[7]);
+		Fdy = Fd * Y[7] / sqrt(Y[6] * Y[6] + Y[7] * Y[7]);
 	}
-	
-	// Рассчет сил трения
-	double Ftr_x = Ftr(mu, m + M, Fx) * vvx;
-	double Ftr_y = Ftr(mu, m + M, Fy) * vvy;
+	else {
+		Fdx = 0;
+		Fdy = 0;
+	}*/
 
-	FY[6] = (-Fx + Ftr_x) / (M); // VX' = -Fупругости_x + Fтрения_x  / M
-	FY[7] = (-Fy + Ftr_y) / (M); // VY' = -Fупругости_y + Fтрения_y  / M
+	double F1x = +k1 * x;
+	double F2x = -k2 * x;
+	double F3x = +k3 * x;
+	double F4x = +k4 * x;
 
-	FY[4] = Fx / m; // Vx' = Fупругости_x / m
-	FY[5] = Fy / m; // Vy' = Fупругости_y / m
-	
+	double F1y = -k1 * y;
+	double F2y = +k2 * y;
+	double F3y = +k3 * y;
+	double F4y = +k4 * y;
+
+	FY[6] = (+F1x + F2x + F3x + F4x + Fdx(mu, M + m, +F1x + F2x + F3x + F4x)) / M;		// VX' = sum FMx
+	FY[7] = (+F1y + F2y + F3y + F4y + Fdy(mu, M + m, +F1y + F2y + F3y + F4y)) / M;		// VY' = sum FMy
+
+	FY[4] = (-F1x - F2x - F3x - F4x - m * FY[6]) / m;	// Vx' = sum Fmx
+	FY[5] = (-F1y - F2y - F3y - F4y - m * FY[7]) / m;	// Vy' = sum Fmy
 
 	return FY;
 }
--- spring-system/springsystem.h
+++ spring-system/springsystem.h
@@ -14,13 +14,13 @@ public:
 	springsys(double m, double M, double k1, double k2, double k3, double k4, double mu, double x0, double y0, double w, double h);
 	~springsys();
 
-	double get_x() { return Y[0]; }
-	double get_y() { return Y[1]; }
+	double get_x() { return Y[0]+ get_X(); }
+	double get_y() { return Y[1]+get_Y(); }
 	double get_X() { return Y[2]; }
 	double get_Y() { return Y[3]; }
 
-	double Fup(double k, double dx) { return k * dx; }
-	double Ftr(double mu, double m, double max);
+	double Fdx(double mu, double m, double max);
+	double Fdy(double mu, double m, double max);
 
 	std::vector<double> get_spring_begin(int num);
 
